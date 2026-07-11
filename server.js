b6ddde16810da444be5113c77dbbace1cc2555f8
manuelarc06 <mrodriguezcardenas@byupathway.edu>
2026-07-11 09:42:02 -0500

    installed swagger

--- server.js
+++ server.js
@@ -2,15 +2,26 @@ const dns = require("node:dns/promises");
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 const express = require("express");
+const bodyParser = require('body-parser');
 
 const mongodb = require('./data/database');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
+
 app.use('/', require('./routes'));
-
-
 
 mongodb.initDb((err) => {
     if (err) {
